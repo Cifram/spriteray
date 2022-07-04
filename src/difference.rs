af6bab094940f3f2169b03e0aa341174422158ea
Aubrey Powell <herufeanor@gmail.com>
2022-07-04 00:02:03 -0700

    Difference SDF now supports up to 6 inputs

--- src/difference.rs
+++ src/difference.rs
@@ -1,8 +1,8 @@
 use glam::Vec3;
 
-use crate::SdfResult;
+use crate::{SdfResult, union2, union3, union4, union5};
 
-pub fn difference2<F1, F2>(pos: Vec3, a: F1, b: F2) -> SdfResult
+pub fn difference2<F1, F2>(pos: Vec3, a: &F1, b: &F2) -> SdfResult
 	where
 		F1: Fn(Vec3) -> SdfResult,
 		F2: Fn(Vec3) -> SdfResult,
@@ -35,4 +35,46 @@ pub fn difference2<F1, F2>(pos: Vec3, a: F1, b: F2) -> SdfResult
 			color: b.color,
 		}
 	}
+}
+
+pub fn difference3<F1, F2, F3>(pos: Vec3, a: &F1, b: &F2, c: &F3) -> SdfResult
+	where
+		F1: Fn(Vec3) -> SdfResult,
+		F2: Fn(Vec3) -> SdfResult,
+		F3: Fn(Vec3) -> SdfResult,
+{
+	difference2(pos, a, &|point| union2(point, b, c))
+}
+
+pub fn difference4<F1, F2, F3, F4>(pos: Vec3, a: &F1, b: &F2, c: &F3, d: &F4) -> SdfResult
+	where
+		F1: Fn(Vec3) -> SdfResult,
+		F2: Fn(Vec3) -> SdfResult,
+		F3: Fn(Vec3) -> SdfResult,
+		F4: Fn(Vec3) -> SdfResult,
+{
+	difference2(pos, a, &|point| union3(point, b, c, d))
+}
+
+pub fn difference5<F1, F2, F3, F4, F5>(pos: Vec3, a: &F1, b: &F2, c: &F3, d: &F4, e: &F5) -> SdfResult
+	where
+		F1: Fn(Vec3) -> SdfResult,
+		F2: Fn(Vec3) -> SdfResult,
+		F3: Fn(Vec3) -> SdfResult,
+		F4: Fn(Vec3) -> SdfResult,
+		F5: Fn(Vec3) -> SdfResult,
+{
+	difference2(pos, a, &|point| union4(point, b, c, d, e))
+}
+
+pub fn difference6<F1, F2, F3, F4, F5, F6>(pos: Vec3, a: &F1, b: &F2, c: &F3, d: &F4, e: &F5, f: &F6) -> SdfResult
+	where
+		F1: Fn(Vec3) -> SdfResult,
+		F2: Fn(Vec3) -> SdfResult,
+		F3: Fn(Vec3) -> SdfResult,
+		F4: Fn(Vec3) -> SdfResult,
+		F5: Fn(Vec3) -> SdfResult,
+		F6: Fn(Vec3) -> SdfResult,
+{
+	difference2(pos, a, &|point| union5(point, b, c, d, e, f))
 }
