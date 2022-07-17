755c86330d315a6594bdcdcc276691eb4dcc86e6
Aubrey Powell <herufeanor@gmail.com>
2022-07-17 12:30:43 -0700

    Move SDFs into modules
    The primitives module includes all the basic shapes. It also includes
    some shapes that are technically a combination of other shapes, like
    truncated cones and capsules.
    
    The modifiers module includes SDFs that take one or more other SDFs and
    modifies this. This is currently just union, difference and transform.

---
+++ src/modifiers/mod.rs
@@ -0,0 +1,7 @@
+mod difference;
+mod transform;
+mod union;
+
+pub use difference::*;
+pub use transform::*;
+pub use union::*;
---
+++ src/primitives/mod.rs
@@ -0,0 +1,15 @@
+mod capsule;
+mod cone;
+mod half_capsule;
+mod plane;
+mod sphere;
+mod torus;
+mod truncated_cone;
+
+pub use capsule::*;
+pub use cone::*;
+pub use half_capsule::*;
+pub use plane::*;
+pub use sphere::*;
+pub use torus::*;
+pub use truncated_cone::*;
