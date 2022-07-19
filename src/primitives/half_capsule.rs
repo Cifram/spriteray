7b9d8fb5749aa2439311b633c79fd23a97624225
Aubrey Powell <herufeanor@gmail.com>
2022-07-18 19:05:29 -0700

    Put the rounded end of half capsule on the bottom
    Half capsules will far more often want to rotate around the rounded end
    than the flat end, so putting the rounded end at the origin makes them
    easier to work with.

--- src/primitives/half_capsule.rs
+++ src/primitives/half_capsule.rs
@@ -3,23 +3,23 @@ use glam::{Vec3, Affine3A};
 use crate::{SdfResult, Color, Sphere, TruncatedCone, Union2, Transform, Sdf};
 
 pub struct HalfCapsule {
-	sdf: Union2<Transform<Sphere>, TruncatedCone>,
+	sdf: Union2<Sphere, Transform<TruncatedCone>>,
 }
 
 impl HalfCapsule {
 	pub fn new(bottom_radius: f32, top_radius: f32, height: f32, color: Color) -> Self {
-		let side_length = (height*height + bottom_radius*bottom_radius - top_radius*top_radius).sqrt();
-		let angle = (height / bottom_radius).atan() + (top_radius / side_length).atan();
-		let angle = if bottom_radius > top_radius { angle } else { std::f32::consts::PI - angle };
+		let side_length = (height*height + top_radius*top_radius - bottom_radius*bottom_radius).sqrt();
+		let angle = (height / top_radius).atan() + (bottom_radius / side_length).atan();
+		let angle = if bottom_radius < top_radius { angle } else { std::f32::consts::PI - angle };
 		let adj_height = angle.sin() * side_length;
-		let adj_top_radius = bottom_radius - angle.cos() * side_length * if bottom_radius > top_radius { 1.0 } else { -1.0 };
+		let adj_bottom_radius = top_radius - angle.cos() * side_length * if bottom_radius < top_radius { 1.0 } else { -1.0 };
 		Self {
 			sdf: Union2::new(
+				Sphere::new(bottom_radius, color),
 				Transform::new(
-					Affine3A::from_translation(Vec3::NEG_Y * height),
-					Sphere::new(top_radius, color),
+					Affine3A::from_translation(Vec3::NEG_Y * (height - adj_height)),
+					TruncatedCone::new(adj_bottom_radius, top_radius, adj_height, color),
 				),
-				TruncatedCone::new(bottom_radius, adj_top_radius, adj_height, color),
 			)
 		}
 	}
