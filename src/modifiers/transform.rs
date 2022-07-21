9fea4128d55b24e989c516054446a54f7b67537c
Aubrey Powell <herufeanor@gmail.com>
2022-07-21 00:30:36 -0700

    Fix normals in transform modifier
    The transform modifier was transforming surface normals improperly. It
    needed to apply the inverse transform instead.

--- src/modifiers/transform.rs
+++ src/modifiers/transform.rs
@@ -3,11 +3,12 @@ use glam::{Affine3A, Vec3};
 use crate::SdfResult;
 
 pub fn transform(transform: Affine3A, sdf: Box<dyn Fn(Vec3) -> SdfResult>) -> Box<dyn Fn(Vec3) -> SdfResult> {
+	let inverse_transform = transform.inverse();
 	Box::new(move |pos| {
 		let result = sdf(transform.transform_point3(pos));
 		SdfResult {
 			range: result.range,
-			normal: transform.transform_vector3(result.normal),
+			normal: inverse_transform.transform_vector3(result.normal).normalize_or_zero(),
 			color: result.color
 		}
 	})
