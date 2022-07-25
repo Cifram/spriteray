7cb18cb21d0968f9092a644af33b1c532e9bfd4b
Aubrey Powell <herufeanor@gmail.com>
2022-07-24 20:31:07 -0700

    Fix a bug in capsule when the radii are equal
    The capsule uses a truncated cone for the middle, and a truncated cone
    where the top and bottom radii are the same is not a truncated cone. The
    truncated cone in this library is literally just the difference between
    a cone and a plane, and it's impossible to construct a cone such that it
    can be cut off with both radii being equal.
    
    So, added the cylinder shape (in the previous commit) and now updated
    capsule to use a cylinder for the middle instead when the radii are
    equal.

--- src/primitives/capsule.rs
+++ src/primitives/capsule.rs
@@ -1,27 +1,38 @@
 use glam::{Vec3, Affine3A};
 
-use crate::{Color, SdfFn, union3, transform, truncated_cone, sphere};
+use crate::{Color, SdfFn, union3, transform, truncated_cone, sphere, cylinder};
 
 pub fn capsule(radius1: f32, radius2: f32, height: f32, color: Color) -> SdfFn {
-	let mut tan_angle = std::f32::consts::PI / 2.0 - ((radius1 - radius2).abs() / height).acos();
-	if radius1 < radius2 {
-		tan_angle = -tan_angle;
-	}
-	let cos_tan_angle = tan_angle.cos();
-	let sin_tan_angle = tan_angle.sin();
-	let cone_radius1 = radius1 * cos_tan_angle;
-	let cone_radius2 = radius2 * cos_tan_angle;
-	let cone_offset1 = radius1 * sin_tan_angle;
-	let cone_offset2 = radius2 * sin_tan_angle;
-	union3(
-		sphere(radius1, color),
-		transform(
-			Affine3A::from_translation(Vec3::NEG_Y * height),
-			sphere(radius2, color),
-		),
-		transform(
-			Affine3A::from_translation(Vec3::NEG_Y * cone_offset1),
-			truncated_cone(cone_radius1, cone_radius2, height - cone_offset1 + cone_offset2, color),
+	if radius1 == radius2 {
+		union3(
+			sphere(radius1, color),
+			transform(
+				Affine3A::from_translation(Vec3::NEG_Y * height),
+				sphere(radius2, color),
+			),
+			cylinder(radius1, height, color),
 		)
-	)
+	} else {
+		let mut tan_angle = std::f32::consts::PI / 2.0 - ((radius1 - radius2).abs() / height).acos();
+		if radius1 < radius2 {
+			tan_angle = -tan_angle;
+		}
+		let cos_tan_angle = tan_angle.cos();
+		let sin_tan_angle = tan_angle.sin();
+		let cone_radius1 = radius1 * cos_tan_angle;
+		let cone_radius2 = radius2 * cos_tan_angle;
+		let cone_offset1 = radius1 * sin_tan_angle;
+		let cone_offset2 = radius2 * sin_tan_angle;
+		union3(
+			sphere(radius1, color),
+			transform(
+				Affine3A::from_translation(Vec3::NEG_Y * height),
+				sphere(radius2, color),
+			),
+			transform(
+				Affine3A::from_translation(Vec3::NEG_Y * cone_offset1),
+				truncated_cone(cone_radius1, cone_radius2, height - cone_offset1 + cone_offset2, color),
+			)
+		)
+	}
 }
