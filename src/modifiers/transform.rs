da066a26ffba536427893003b9f3a00ae3bbaa2f
Aubrey Powell <herufeanor@gmail.com>
2022-07-26 19:52:59 -0700

    Add support for non-uniform scales
    Transform wasn't previously transforming surface normals in a way which
    worked for non-uniform scales. It needs to invert the rotation but not
    the scale, and apply the scale after the rotation, when transforming a
    normal.

--- src/modifiers/transform.rs
+++ src/modifiers/transform.rs
@@ -3,12 +3,12 @@ use glam::{Affine3A, Vec3};
 use crate::SdfResult;
 
 pub fn transform(transform: Affine3A, sdf: Box<dyn Fn(Vec3) -> SdfResult>) -> Box<dyn Fn(Vec3) -> SdfResult> {
-	let inverse_transform = transform.inverse();
+	let (scale, rotation, _) = transform.to_scale_rotation_translation();
 	Box::new(move |pos| {
 		let result = sdf(transform.transform_point3(pos));
 		SdfResult {
 			range: result.range,
-			normal: inverse_transform.transform_vector3(result.normal).normalize_or_zero(),
+			normal: (rotation.inverse() * result.normal) * scale,
 			color: result.color
 		}
 	})
